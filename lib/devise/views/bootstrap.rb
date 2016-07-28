b7f25bd0346724bd68e7856393e39eeb7551da6b
Oleg A. Bavaev <jesoba7@gmail.com>
2016-07-29 07:56:05 +1000

    First commit

---
+++ lib/devise/views/bootstrap.rb
@@ -0,0 +1,9 @@
+require "devise/views/bootstrap/version"
+
+module Devise
+  module Views
+    module Bootstrap
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/devise/views/bootstrap/version.rb
@@ -0,0 +1,7 @@
+module Devise
+  module Views
+    module Bootstrap
+      VERSION = "0.1.0"
+    end
+  end
+end
